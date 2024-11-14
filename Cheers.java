3d4b7d95b3d33682249b8b946e95c420f0937ade
AmitErlich7 <amit.erlich@post.runi.ac.il>
2024-11-14 17:23:32 +0200

    Cheers file 1st

--- Cheers.java
+++ Cheers.java
@@ -1,6 +1,22 @@
-// Prints a crowd cheering output.
 public class Cheers {
         public static void main(String[] args) {
-	    // Replace this comment with your code
+                String name = args[0];
+                name = name.toUpperCase();
+                int wordLength = name.length();
+                int numOfTimes = Integer.parseInt(args[1]);
+                int j = 0;
+                for (int i = 0; i < wordLength; i++) {
+                        char currentChar = name.charAt(j);
+                        if (currentChar == 'A' || currentChar == 'E' || currentChar == 'F' || currentChar == 'H' || currentChar == 'I' || currentChar == 'L' || currentChar == 'M' || currentChar == 'N' || currentChar == 'O' || currentChar == 'R' || currentChar == 'S' || currentChar == 'X') {
+                               System.out.println("Give me an " + currentChar + ": " + currentChar + "!");                     
+                        }
+                        else {
+                                System.out.println("Give me a " + currentChar + ": " + currentChar + "!");
+                        }
+                        j++;
+                }
+                for (int k = 0; k < numOfTimes; k++) {
+                        System.out.println(name + "!!!");
+                }
         }
 }
